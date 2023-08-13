0dd5357a1d68c0856db1535afcdb74d74dfeef6d
Kenny Lee <kenny.lee927@gmail.com>
2023-08-13 18:49:14 -0400

    split into index and module, refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,23 @@
 const request = require("request");
-// Capture node command line argument of search query
-const query = process.argv[2];
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${query}`,
-  (error, response, body) => {
-    // Print the error if one occurred
-    if (error) {
-      console.log("error:", error);
-      return;
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      // Print the error if one occurred
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      const data = JSON.parse(body);
+      // if any search results exist, there will be a first element in the array
+      if (data[0]) {
+        callback(null, data[0].description);
+      } else {
+        callback(null, "No results found!");
+      }
     }
-    const data = JSON.parse(body);
-    // if any search results exist, there will be a first element in the array
-    if (data[0]) {
-      console.log(data[0].description);
-    } else {
-      console.log("No results found!");
-    }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
