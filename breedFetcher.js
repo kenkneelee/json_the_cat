b76e3e4af5412c8e31528914bde8c0d095fff434
Kenny Lee <kenny.lee927@gmail.com>
2023-08-13 18:05:37 -0400

    add edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,20 @@
 const request = require("request");
-const query = process.argv[2]
+// Capture node command line argument of search query
+const query = process.argv[2];
 request(
   `https://api.thecatapi.com/v1/breeds/search?q=${query}`,
   (error, response, body) => {
+    // Print the error if one occurred
     if (error) {
-      console.log("error:", error); // Print the error if one occurred
+      console.log("error:", error);
+      return;
     }
     const data = JSON.parse(body);
-    console.log(data[0].description);
-    // console.log(typeof data);
+    // if any search results exist, there will be a first element in the array
+    if (data[0]) {
+      console.log(data[0].description);
+    } else {
+      console.log("No results found!");
+    }
   }
 );
